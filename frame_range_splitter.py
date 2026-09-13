28120b21e457bdab9daafe5d10c10d1e685877d6
Sh I <shanismail11@gmail.com>
2026-09-13 16:19:39 -0500

    added base panel code

--- frame_range_splitter.py
+++ frame_range_splitter.py
@@ -1,12 +1,27 @@
 bl_info = {
     "name": "Frame Range Splitter",
-    "blender": "2,80,0",
+    "blender": (2,80,0),
 }
 
 
+import bpy
 
-def register():
-    print("hello")
+class VIEW3D_PT_frame_range_splitter(bpy.types.Panel):
+    """frame range splitter"""
+    bl_idname = "VIEW3D_PT_frame_range_splitter" 
+    bl_label = "Frame Range Splitter"
 
-def unregister():
-    print("goodbye")
+    #define where in blender UI it will be
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Frame Range Splitter"
+
+    
+    def draw(self, context):
+        #add a label
+        self.layout.label(text="Hello World")
+
+#register panel so it will be displayed
+bpy.utils.register_class(VIEW3D_PT_frame_range_splitter)
+
+
